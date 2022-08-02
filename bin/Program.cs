7d73ee60f8e62526850857e421fc39a379733c16
SailorStarHealer <108467609+SailorStarHealer@users.noreply.github.com>
2022-08-02 11:40:15 +0200

    Update Program.cs

--- bin/Program.cs
+++ bin/Program.cs
@@ -1,48 +1,17 @@
 ﻿static class Program
 {
     static SortedList<string, int> playerScore = new SortedList<string, int>();
-    static void Dictionary1()
-    {// See https://aka.ms/new-console-template for more information
-        /*
-         * 
-        IDictionary<int, string> hostCountries = new Dictionary<int, string>();
-        hostCountries[2000] = "Australia";
-        hostCountries[2004] = "Greece";
-        hostCountries[2008] = "China";
-        hostCountries[2012] = "Britain";
-        hostCountries[2016] = "Brazil";
-        hostCountries[2020] = "Japan";
-        hostCountries[2024] = "France";
-        hostCountries[2028] = "North America";
+    static void countryQuiz()
+    {
 
-        var random = new Random();
 
-        int year = 2000 + random.Next(hostCountries.Count) * 4;
-
-        Console.WriteLine($"Which country hosted the summer olympics in {year}?");
-
-        string answer = Console.ReadLine();
-
-        //Console.WriteLine(hostCountries[year]);
-
-        if (hostCountries[year] == (answer))
-        {
-            Console.WriteLine("Correct answer!");
-        }
-        else
-        {
-            Console.WriteLine($"Too bad. The correct answer was");
-            Console.Write(hostCountries[year]);
-        } */
+        //Mission 2
 
         //Creates a new Dictionary full of countries and their names.
         //Each capital is assigned to a country value.
         //It is made out of Strings as a SortedList.
 
-        //Mission 2
-
-
-        IDictionary<string, string> countryCapitals = new SortedList<string, string>();
+        SortedList<string, string> countryCapitals = new SortedList<string, string>();
         countryCapitals["India"] = "New Dehli";
         countryCapitals["Palau"] = "Ngerulmud";
         countryCapitals["Niger"] = "Niamey";
@@ -56,8 +25,7 @@
         //Creates a new random engine.
         var random = new Random();
 
-        //Generates a new list of strings by copying the dictionary,
-        //pulls a random from them.
+        //Generates a new list of strings by copying the dictionary, pulls a random from them.
         int sort = random.Next(0, 8);
         var countries = new List<string>(countryCapitals.Keys);
 
@@ -70,10 +38,7 @@
         //Reads the answer that the user generates and assigns it a string value.
         string answer = Console.ReadLine();
 
-        //Writes out the answer to the question in 'countries'
-        Console.WriteLine(countryCapitals[country]);
-
-        //Outputs correct answer if Readline value corepponds to 
+        //Outputs correct answer if Readline value is not correct.
         if (countryCapitals[country] == (answer))
         {
             Console.WriteLine("Correct answer!");
@@ -88,16 +53,15 @@
 
     static bool Player()
     {
-        // Input name using readline
+        // Input name using readline.
         Console.WriteLine("Please enter a name");
         string playerName = Console.ReadLine();
 
-        //to create a new dictionary element with an assigned value of one.
+        //To create a new dictionary element with an assigned value of one.
 
         // If loop.
         //The starting condition is that the pre-assigned custom value playerName is inputted into the Console.Readline.
-        if
-        (playerScore.ContainsKey(playerName))
+        if (playerScore.ContainsKey(playerName))
         {
             //This means that the value at that point in the dictionary increases by one.
             playerScore[playerName] += 1;
@@ -108,7 +72,7 @@
             playerScore[playerName] = 1;
         }
 
-        //for each playername in playerScore, output a list of playerName + score
+        //For each playername in playerScore, output a list of playerName + score.
 
         for (int i = 0; i < playerScore.Count; i++)
         {
@@ -126,11 +90,11 @@
         do
         {
             Console.WriteLine("Hello!");
-            Dictionary1();
+            countryQuiz();
             isPlayerNameEmpty = Player();
         }
 
-        while (isPlayerNameEmpty);
+        while (isPlayerNameEmpty == true);
 
         Console.WriteLine("Good bye!");
     }
